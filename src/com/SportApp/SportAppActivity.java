b2cb7ac61f9cd659b985f382d1ada018f8aecf17
vtemian <vladtemian@gmail.com>
2011-06-21 23:34:36 +0300

    Implemeted facebook-login with facebook sdk for android

--- src/com/SportApp/SportAppActivity.java
+++ src/com/SportApp/SportAppActivity.java
@@ -1,12 +1,35 @@
 package com.SportApp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.facebook.android.*;
+import com.facebook.android.Facebook.*;
 
 public class SportAppActivity extends Activity {
 	
+	Facebook facebook = new Facebook("208581092517993");
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        facebook.authorize(this, new DialogListener() {
+            public void onComplete(Bundle values) {}
+
+            public void onFacebookError(FacebookError error) {}
+
+            public void onError(DialogError e) {}
+
+            public void onCancel() {}
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        facebook.authorizeCallback(requestCode, resultCode, data);
     }
 }
